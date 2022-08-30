981bba7a62a75bbe128b1b249b4cc96c46f49b7d
upar1234 <upar1234@gmail.com>
2022-08-29 20:20:33 -0500

    :+1:
    0

---
+++ televisores/marca.py
@@ -0,0 +1,38 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class Marca:\n",
+    "    def __init__(self, nombre):\n",
+    "        self.nombre=nombre\n",
+    "    def getNombre(self):\n",
+    "        return self.nombre\n",
+    "\n",
+    "\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.10.6 64-bit",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.10.6"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "7579aee364a2e290dfc6a620616239004625e0c1356cc42c7bb47894a11803af"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
