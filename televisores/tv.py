5262bc7b8d74609f4c3e9c898d8f45059c5f4343
upar1234 <upar1234@gmail.com>
2022-08-29 23:56:54 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -28,7 +28,7 @@ class TV:
     def getCanal(self):
         return self.canal
     def setCanal(self, canal):
-        if(self.estado == True):
+        if(self.estado == True) and (self.canal > 0 and self.canal <= 120):
             self.canal=canal
     @classmethod
     def setNumTV(self, numTV):
