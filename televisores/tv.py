78b157f1a8777090ddcd4d76c8176edad0fcf250
upar1234 <upar1234@gmail.com>
2022-08-29 23:45:31 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -41,16 +41,16 @@ class TV:
     def turnOff(self):
         if estado ==True:
             estado = False
-    def canalUp(self, canal):
+    def canalUp(self):
         if (canal > 0 and canal <= 120)and(self.estado==True):
             self.canal+=1
-    def canalDown(self, canal):
+    def canalDown(self):
         if (canal > 1 and canal < 120)and(self.estado==True):
             self.canal-=1
-    def volumenUp(self, volumen):
+    def volumenUp(self):
         if (volumen >=0 and volumen <=7)and(self.estado==True):
             self.volumen+=1
-    def volumenDown(self, volumen):
+    def volumenDown(self):
         if (volumen < 8 and volumen >=0)and(self.estado == True):
             self.volumen-=1
     def getEstado(self):
